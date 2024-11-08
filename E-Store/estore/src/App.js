5fca9408b42575c370083bb134f0e3759891dc6b
ABINASH BEHERA <128899808+lucky-1-4-3@users.noreply.github.com>
2024-11-09 01:00:39 +0530

    .

--- E-Store/estore/src/App.js
+++ E-Store/estore/src/App.js
@@ -1,14 +1,21 @@
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-       
-        
-      </header>
+      <div className='header bg-dark'>
+        <div className='row'>
+          <div className='brand my-1'>
+            <h1> eStore </h1>
+          </div>
+          <div className='inp-container p-0 my-4 w-50 h-25 bg-white'>
+            <input className='form-control ' placeholder='Search...'/>
+            <button> <i className='fa fa-search'/> </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
